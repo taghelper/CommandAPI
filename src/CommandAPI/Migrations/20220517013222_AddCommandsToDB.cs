fe3027bf14300a2f3f4bb6e8c53703cc0b618d49
Hossein Dara <drhossein501@yahoo.com>
2022-05-18 10:47:45 +0430

    Added appsettings info-CH7

---
+++ src/CommandAPI/Migrations/20220517013222_AddCommandsToDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CommandAPI.Migrations
+{
+    public partial class AddCommandsToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    HowTo = table.Column<string>(maxLength: 250, nullable: false),
+                    Platform = table.Column<string>(nullable: false),
+                    CommandLine = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+        }
+    }
+}
---
+++ src/CommandAPI/Migrations/20220517025718_Edit-2-CommandsToDB.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommandAPI.Migrations
+{
+    public partial class Edit2CommandsToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Commands",
+                table: "Commands");
+
+            migrationBuilder.RenameTable(
+                name: "Commands",
+                newName: "CommandItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CommandItems",
+                table: "CommandItems",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CommandItems",
+                table: "CommandItems");
+
+            migrationBuilder.RenameTable(
+                name: "CommandItems",
+                newName: "Commands");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Commands",
+                table: "Commands",
+                column: "Id");
+        }
+    }
+}
